96fdf82eb7b4dd9846a83e2e0babffbaeddceeaa
maisvendoo <maisvendoo@yandex.ru>
2024-07-01 00:55:28 +0300

    программируемые стадии графического конвейера

--- vkengine/src/vk_engine_create_graphics_pipeline.cpp
+++ vkengine/src/vk_engine_create_graphics_pipeline.cpp
@@ -8,11 +8,31 @@
 //------------------------------------------------------------------------------
 void VulkanEngine::create_graphics_pipeline()
 {
+    // Загружаем код вершинного и фрагментного шейдеров
     auto vertShaderCode = readSPV("../shaders/shader.vert.spv");
     auto fragShaderCode = readSPV("../shaders/shader.frag.spv");
 
+    // Создаем шейдерные модули
     VkShaderModule vertShaderModule = create_shader_module(vertShaderCode);
     VkShaderModule fragShaderModule = create_shader_module(fragShaderCode);
+
+    VkPipelineShaderStageCreateInfo vertShaderStageInfo = {};
+    vertShaderStageInfo.sType = VK_STRUCTURE_TYPE_PIPELINE_SHADER_STAGE_CREATE_INFO;
+    vertShaderStageInfo.pNext = nullptr;
+    vertShaderStageInfo.stage = VK_SHADER_STAGE_VERTEX_BIT;
+    vertShaderStageInfo.module = vertShaderModule;
+    vertShaderStageInfo.pName = "main";
+
+    VkPipelineShaderStageCreateInfo fragShaderStageInfo = {};
+    fragShaderStageInfo.sType = VK_STRUCTURE_TYPE_PIPELINE_SHADER_STAGE_CREATE_INFO;
+    fragShaderStageInfo.pNext = nullptr;
+    fragShaderStageInfo.stage = VK_SHADER_STAGE_FRAGMENT_BIT;
+    fragShaderStageInfo.module = fragShaderModule;
+    fragShaderStageInfo.pName = "main";
+
+    VkPipelineShaderStageCreateInfo shaderStages[] = {
+        vertShaderStageInfo, fragShaderStageInfo
+    };
 
     vkDestroyShaderModule(device, fragShaderModule, nullptr);
     vkDestroyShaderModule(device, vertShaderModule, nullptr);
